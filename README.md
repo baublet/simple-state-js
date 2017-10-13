d4929e939d7d04ec4518eef4a8b658c9f4b3378c
Ryan M. Poe <rmp23@duke.edu>
2017-10-13 13:43:32 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,57 @@
+SimpleStore.js
+
+SimpleStore.js is a dead simple state manager for JavaScript and Node. It's intended
+to be isomorphic, discreet, and there for you in a pinch when more robust state managers
+like Redux are too much for the project.
+
+# API
+
+SimpleStore.js exports a function that returns a state manager. That's it. That
+manager controls its own watchers, the store, and all of the other pageantry you
+may expect in such a tool.
+
+```js
+import createStore from "simple-state-js"
+
+const myState = createStore()
+
+// Optional, if you're in the browser
+document.appState = myState
+
+// To set a node
+myState.set('myKey', "Hello, sir!")
+
+// To get that node
+console.log(myState.get('myKey'))   // -> Hello, sir!
+
+// To watch a node for changes
+myState.watch('myKey', (newState, oldState) => {
+    console.log(newState)
+})
+
+myState.set('myKey', "Foo bar.")    // -> Foo bar.
+```
+
+SimpleStore.js relies on `node-deep-equal` to compare objects that are passed in
+as key values to decide when to update the state and watchers. Therefore, if your
+state key is a fairly large object and you need to fully replace it (e.g., from
+an API request), you can safely do so without having to worry about everything
+updating.
+
+## Watchers
+
+Watchers are stored by a key and value so that they can be removed or replaced.
+
+```js
+myState.set('myKey', 'Foo bar!')
+
+myState.watch('myKey', newState => {
+    console.log('Watcher is on!')
+}, 'myWatcher')
+
+myState.set('myKey', "Hello world")     // -> Watcher is on!
+
+myState.unWatch('myWatcher')
+
+myState.set('myKey', "Foo bar!")        // ->
+```
---
+++ src/test.js
@@ -0,0 +1,60 @@
+import createStore from "./"
+
+test('creates a store', () => {
+    const store = createStore()
+    expect(typeof store).toBe("object");
+})
+
+test('gets and sets properly', () => {
+    const store = createStore()
+    expect(store.get('myKey')).toBe(undefined)
+    store.set('myKey', 'Test')
+    expect(store.get('myKey')).toBe('Test')
+    store.set('myKey', 'Foo')
+    expect(store.get('myKey')).toBe('Foo')
+})
+
+test('creates a store with default state', () => {
+    const store = createStore({ myKey: 'Test' })
+    expect(store.get('myKey')).toBe('Test')
+})
+
+test('allows watchers to be set and used', () => {
+    const store = createStore({ myKey: 'Test' })
+    store.watch('myKey', newState => {
+        expect(newState).toBe('Foo')
+    })
+    store.set('myKey', 'Foo')
+})
+
+test('watchers are not called if state is unchanged', done => {
+    const store = createStore({ myKey: 'Test' })
+    store.watch('myKey', newState => {
+        expect(false).toBe(true)
+    })
+    store.set('myKey', 'Test')
+    done()
+})
+
+test('watchers can be unwatched', () => {
+    const store = createStore({ myKey: 'Test' })
+    store.watch('myKey', () => {})
+    expect(store._watchers['myKey'].length).toBe(1)
+    store.watch('myKey', newState => {
+        expect(false).toBe(true)
+    }, 'myWatcher')
+    expect(store._watchers['myKey'].length).toBe(2)
+    store.unwatch('myKey', 'myWatcher')
+    expect(store._watchers['myKey'].length).toBe(1)
+    expect(store._watchers['myKey'][0][0]).toBe(false)
+})
+
+test('watchers can be fully unwatched', () => {
+    const store = createStore({ myKey: 'Test' })
+    store.watch('myKey', () => {})
+    store.watch('myKey', () => {})
+    store.watch('myKey', () => {})
+    expect(store._watchers['myKey'].length).toBe(3)
+    store.unwatch('myKey')
+    expect(store._watchers['myKey'].length).toBe(0)
+})
